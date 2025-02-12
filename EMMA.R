1d46dde55521e700b8f5bf85ed544e9630a18b93
Bryce Shoup <bshoup@uwyo.edu>
2025-02-12 11:31:52 -0700

    comit from r

--- EMMA.R
+++ EMMA.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(readxl)
 library(GGally)
-all.2023.anions <- read_excel("~/Documents/Data/2023_Data/Ion.Data/BryceTetonH2O_052024.xlsx", sheet = 1, skip = 3) %>% 
-  subset(select =c(1:8)) %>% 
-  rename(SITE = ...1) %>% 
+all.2023.anions <- read_excel("~/Documents/Data/2023_Data/Ion.Data/BryceTetonH2O_052024.xlsx", sheet = 1, skip = 3) %>%
+  subset(select =c(1:8)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Anion")) ~ "lab",
@@ -17,12 +17,12 @@ all.2023.anions <- read_excel("~/Documents/Data/2023_Data/Ion.Data/BryceTetonH2O
     str_detect(SITE, regex("Inbre")) ~ "lab",
     str_detect(SITE, regex("Whirl")) ~ "lab",
     str_detect(SITE, regex("GV")) ~ "lab",
-    TRUE ~ "RG")) %>% 
+    TRUE ~ "RG")) %>%
   filter(Setting.Type == "RG")
 
-june.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 1, skip = 3) %>% 
-  subset(select =c(1:8)) %>% 
-  rename(SITE = ...1) %>% 
+june.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 1, skip = 3) %>%
+  subset(select =c(1:8)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Anion")) ~ "lab",
@@ -32,12 +32,12 @@ june.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2
     str_detect(SITE, regex("FB")) ~ "lab",
     str_detect(SITE, regex("Snow")) ~ "lab",
     str_detect(SITE, regex("Field")) ~ "lab",
-    TRUE ~ "RG")) %>% 
+    TRUE ~ "RG")) %>%
   filter(Setting.Type == "RG")
 
-july.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 3, skip = 3) %>% 
-  subset(select =c(1:8)) %>% 
-  rename(SITE = ...1) %>% 
+july.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 3, skip = 3) %>%
+  subset(select =c(1:8)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Anion")) ~ "lab",
@@ -47,12 +47,12 @@ july.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2
     str_detect(SITE, regex("MDL")) ~ "lab",
     str_detect(SITE, regex("FB")) ~ "lab",
     str_detect(SITE, regex("SP")) ~ "lab",
-    TRUE ~ "RG")) %>% 
-  filter(Setting.Type == "RG")    
+    TRUE ~ "RG")) %>%
+  filter(Setting.Type == "RG")
 
-august.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 5, skip = 3) %>% 
-  subset(select =c(1:8)) %>% 
-  rename(SITE = ...1) %>% 
+august.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 5, skip = 3) %>%
+  subset(select =c(1:8)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Anion")) ~ "lab",
@@ -61,12 +61,12 @@ august.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTeton
     str_detect(SITE, regex("SO")) ~ "lab",
     str_detect(SITE, regex("MDL")) ~ "lab",
     str_detect(SITE, regex("FB")) ~ "lab",
-    TRUE ~ "RG")) %>% 
-  filter(Setting.Type == "RG") 
+    TRUE ~ "RG")) %>%
+  filter(Setting.Type == "RG")
 
-august.2024.anions.leftover <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 9, skip = 3) %>% 
-  subset(select =c(1:8)) %>% 
-  rename(SITE = ...1) %>% 
+august.2024.anions.leftover <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 9, skip = 3) %>%
+  subset(select =c(1:8)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Anion")) ~ "lab",
@@ -75,12 +75,12 @@ august.2024.anions.leftover <- read_excel("~/Documents/Data/2024_Data/Ion.Data/B
     str_detect(SITE, regex("SO")) ~ "lab",
     str_detect(SITE, regex("MDL")) ~ "lab",
     str_detect(SITE, regex("FB")) ~ "lab",
-    TRUE ~ "RG")) %>% 
-  filter(Setting.Type == "RG") 
+    TRUE ~ "RG")) %>%
+  filter(Setting.Type == "RG")
 
-september.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 7, skip = 3) %>% 
-  subset(select =c(1:8)) %>% 
-  rename(SITE = ...1) %>% 
+september.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 7, skip = 3) %>%
+  subset(select =c(1:8)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Anion")) ~ "lab",
@@ -89,33 +89,33 @@ september.2024.anions <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTe
     str_detect(SITE, regex("SO")) ~ "lab",
     str_detect(SITE, regex("MDL")) ~ "lab",
     str_detect(SITE, regex("FB")) ~ "lab",
-    TRUE ~ "RG")) %>% 
-  filter(Setting.Type == "RG") 
+    TRUE ~ "RG")) %>%
+  filter(Setting.Type == "RG")
 
-anions <- all.2023.anions %>% 
-  bind_rows(june.2024.anions) %>% 
-  bind_rows(july.2024.anions) %>% 
-  bind_rows(august.2024.anions) %>% 
-  bind_rows(august.2024.anions.leftover) %>% 
-  bind_rows(september.2024.anions) %>% 
-  separate(SITE, c("location", "Event"), remove = FALSE) %>% 
-  subset(select = -c(Setting.Type)) %>% 
+anions <- all.2023.anions %>%
+  bind_rows(june.2024.anions) %>%
+  bind_rows(july.2024.anions) %>%
+  bind_rows(august.2024.anions) %>%
+  bind_rows(august.2024.anions.leftover) %>%
+  bind_rows(september.2024.anions) %>%
+  separate(SITE, c("location", "Event"), remove = FALSE) %>%
+  subset(select = -c(Setting.Type)) %>%
   mutate(Event = as.numeric(Event)) #no observable bromide
 
-anions.nitrate <- anions %>% 
+anions.nitrate <- anions %>%
   #Calculating the concentration in 14 mL
   mutate(nitrate.15 = nitrate / 0.014)
 
-anions.longer <- anions %>% 
-  pivot_longer(c(fluoride:sulfate), names_to = "anion", values_to = "ppm") %>% 
-  group_by(anion) %>% 
+anions.longer <- anions %>%
+  pivot_longer(c(fluoride:sulfate), names_to = "anion", values_to = "ppm") %>%
+  group_by(anion) %>%
   mutate(ppm = as.numeric(ppm))
-
+write_csv(anions, "~/Documents/Data/Chapter.1/ion_data/anions.csv")
 rm(all.2023.anions, august.2024.anions,august.2024.anions.leftover,september.2024.anions,july.2024.anions,june.2024.anions)
 ####Cations###
-all.2023.cations <- read_excel("~/Documents/Data/2023_Data/Ion.Data/BryceTetonH2O_052024.xlsx", sheet = 2, skip = 3) %>% 
-  subset(select =c(1:6)) %>% 
-  rename(SITE = ...1) %>% 
+all.2023.cations <- read_excel("~/Documents/Data/2023_Data/Ion.Data/BryceTetonH2O_052024.xlsx", sheet = 2, skip = 3) %>%
+  subset(select =c(1:6)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Cation")) ~ "lab",
@@ -129,12 +129,12 @@ all.2023.cations <- read_excel("~/Documents/Data/2023_Data/Ion.Data/BryceTetonH2
     str_detect(SITE, regex("Inbre")) ~ "lab",
     str_detect(SITE, regex("Whirl")) ~ "lab",
     str_detect(SITE, regex("GV")) ~ "lab",
-    TRUE ~ "RG")) %>% 
+    TRUE ~ "RG")) %>%
   filter(Setting.Type == "RG")
 
-june.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 2, skip = 3) %>% 
-  subset(select =c(1:6)) %>% 
-  rename(SITE = ...1) %>% 
+june.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 2, skip = 3) %>%
+  subset(select =c(1:6)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Cation")) ~ "lab",
@@ -144,12 +144,12 @@ june.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH
     str_detect(SITE, regex("FB")) ~ "lab",
     str_detect(SITE, regex("Snow")) ~ "lab",
     str_detect(SITE, regex("Field")) ~ "lab",
-    TRUE ~ "RG")) %>% 
+    TRUE ~ "RG")) %>%
   filter(Setting.Type == "RG")
 
-july.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 4, skip = 3) %>% 
-  subset(select =c(1:6)) %>% 
-  rename(SITE = ...1) %>% 
+july.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 4, skip = 3) %>%
+  subset(select =c(1:6)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Cation")) ~ "lab",
@@ -159,26 +159,12 @@ july.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH
     str_detect(SITE, regex("MDL")) ~ "lab",
     str_detect(SITE, regex("FB")) ~ "lab",
     str_detect(SITE, regex("SP")) ~ "lab",
-    TRUE ~ "RG")) %>% 
-  filter(Setting.Type == "RG")    
-
-august.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 6, skip = 3) %>% 
-  subset(select =c(1:6)) %>% 
-  rename(SITE = ...1) %>% 
-  mutate(Setting.Type = case_when(
-    str_detect(SITE, regex("DI")) ~ "lab",
-    str_detect(SITE, regex("Cation")) ~ "lab",
-    str_detect(SITE, regex("Standard")) ~ "lab",
-    str_detect(SITE, regex("QC")) ~ "lab",
-    str_detect(SITE, regex("SO")) ~ "lab",
-    str_detect(SITE, regex("MDL")) ~ "lab",
-    str_detect(SITE, regex("FB")) ~ "lab",
-    TRUE ~ "RG")) %>% 
+    TRUE ~ "RG")) %>%
   filter(Setting.Type == "RG")
 
-august.2024.cations.leftover <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 10, skip = 3) %>% 
-  subset(select =c(1:6)) %>% 
-  rename(SITE = ...1) %>% 
+august.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 6, skip = 3) %>%
+  subset(select =c(1:6)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Cation")) ~ "lab",
@@ -187,12 +173,12 @@ august.2024.cations.leftover <- read_excel("~/Documents/Data/2024_Data/Ion.Data/
     str_detect(SITE, regex("SO")) ~ "lab",
     str_detect(SITE, regex("MDL")) ~ "lab",
     str_detect(SITE, regex("FB")) ~ "lab",
-    TRUE ~ "RG")) %>% 
-  filter(Setting.Type == "RG") 
+    TRUE ~ "RG")) %>%
+  filter(Setting.Type == "RG")
 
-september.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 8, skip = 3) %>% 
-  subset(select =c(1:6)) %>% 
-  rename(SITE = ...1) %>% 
+august.2024.cations.leftover <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 10, skip = 3) %>%
+  subset(select =c(1:6)) %>%
+  rename(SITE = ...1) %>%
   mutate(Setting.Type = case_when(
     str_detect(SITE, regex("DI")) ~ "lab",
     str_detect(SITE, regex("Cation")) ~ "lab",
@@ -201,26 +187,40 @@ september.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceT
     str_detect(SITE, regex("SO")) ~ "lab",
     str_detect(SITE, regex("MDL")) ~ "lab",
     str_detect(SITE, regex("FB")) ~ "lab",
-    TRUE ~ "RG")) %>% 
-  filter(Setting.Type == "RG") 
+    TRUE ~ "RG")) %>%
+  filter(Setting.Type == "RG")
 
-cations <- all.2023.cations %>% 
-  bind_rows(june.2024.cations) %>% 
-  bind_rows(july.2024.cations) %>% 
-  bind_rows(august.2024.cations.leftover) %>% 
-  bind_rows(september.2024.cations) %>% 
-  separate(SITE, c("location", "Event")) %>% 
-  subset(select = -c(Setting.Type)) %>% 
+september.2024.cations <- read_excel("~/Documents/Data/2024_Data/Ion.Data/BryceTetonH2O_names_adjusted.xlsx", sheet = 8, skip = 3) %>%
+  subset(select =c(1:6)) %>%
+  rename(SITE = ...1) %>%
+  mutate(Setting.Type = case_when(
+    str_detect(SITE, regex("DI")) ~ "lab",
+    str_detect(SITE, regex("Cation")) ~ "lab",
+    str_detect(SITE, regex("Standard")) ~ "lab",
+    str_detect(SITE, regex("QC")) ~ "lab",
+    str_detect(SITE, regex("SO")) ~ "lab",
+    str_detect(SITE, regex("MDL")) ~ "lab",
+    str_detect(SITE, regex("FB")) ~ "lab",
+    TRUE ~ "RG")) %>%
+  filter(Setting.Type == "RG")
+
+cations <- all.2023.cations %>%
+  bind_rows(june.2024.cations) %>%
+  bind_rows(july.2024.cations) %>%
+  bind_rows(august.2024.cations.leftover) %>%
+  bind_rows(september.2024.cations) %>%
+  separate(SITE, c("location", "Event")) %>%
+  subset(select = -c(Setting.Type)) %>%
   mutate(Event = as.numeric(Event))
 
-cations.longer <- cations %>% 
-  pivot_longer(c(Sodium:Calcium), names_to = "cation", values_to = "ppm") %>% 
-  group_by(cation) %>% 
+cations.longer <- cations %>%
+  pivot_longer(c(Sodium:Calcium), names_to = "cation", values_to = "ppm") %>%
+  group_by(cation) %>%
   mutate(ppm = as.numeric(ppm))
 
 rm(all.2023.cations, august.2024.cations,august.2024.cations.leftover,september.2024.cations,july.2024.cations,june.2024.cations)
-
-# ggplot(data = cations.longer, aes(x= location,y=ppm )) +
+write_csv(cations, "~/Documents/Data/Chapter.1/ion_data/pb_cations.csv")
+ggplot(data = cations.longer, aes(x= location,y=ppm )) +
 #   geom_boxplot() +
 #   facet_wrap(~cation, scales = "free_y")
 
@@ -252,20 +252,20 @@ po4d<-0.01031
 so4d<-0.02082
 
 #Calculating meq with the above values and by the full method
-fl <- all.2023.anions$fluoride* fd
-fl2 <- (all.2023.anions$fluoride*1)/18.998
-cl<- all.2023.anions$chloride*cld
-cl2<- (all.2023.anions$chloride*1)/35.453
-no2 <- all.2023.anions$nitrite * no2d
-no22 <- (all.2023.anions$nitrite * 1) / 46.005
-br <- all.2023.anions$bromide * brd
-br2 <- (all.2023.anions$bromide * 1) /79.904
-no3 <- all.2023.anions$nitrate * no3d
-no32 <- (all.2023.anions$nitrate*1)/62.0049
-po4 <- all.2023.anions$phosphate * po4d
-po42 <- (all.2023.anions$phosphate * 3) / 94.9714
-so4<- all.2023.anions$sulfate * so4d
-so42 <- (all.2023.anions$sulfate * 2)/96.06
+fl <- anions$fluoride* fd
+fl2 <- (anions$fluoride*1)/18.998
+cl<- anions$chloride*cld
+cl2<- (anions$chloride*1)/35.453
+no2 <- anions$nitrite * no2d
+no22 <- (anions$nitrite * 1) / 46.005
+br <- anions$bromide * brd
+br2 <- (anions$bromide * 1) /79.904
+no3 <- anions$nitrate * no3d
+no32 <- (anions$nitrate*1)/62.0049
+po4 <- anions$phosphate * po4d
+po42 <- (anions$phosphate * 3) / 94.9714
+so4<- anions$sulfate * so4d
+so42 <- (anions$sulfate * 2)/96.06
 
 #Filtering out samples less than 1 ppm
 anion.meq <- anions %>% 
@@ -284,7 +284,7 @@ anion.meq <- anion.meq %>%
           nitrate.15 = nitrate / 0.014)
 #adding the meq to check if it goes above the resin value
 anion.meq<-anion.meq %>% 
- dplyr::mutate("SUM_RQ" = rowSums((anion.meq[,3:9]), na.rm = TRUE)) %>% 
+ dplyr::mutate("meq" = rowSums((anion.meq[,3:9]), na.rm = TRUE)) %>% 
   mutate(Event = as.numeric(Event)) %>% 
   filter(Event >=10)
 
